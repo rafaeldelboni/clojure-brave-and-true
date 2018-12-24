e69e1195e0df2c8b8480a590f9013f126a061895
Rafael Delboni <rafadelboni@gmail.com>
2018-12-24 14:20:33 +0000

    Feat adds chapter5

---
+++ src/clojure_brave_and_true/chapter5.clj
@@ -0,0 +1,44 @@
+(ns clojure-brave-and-true.chapter5)
+
+(def character
+  {:name "Smooches McCutes"
+   :attributes {:intelligence 10
+                :strength 4
+                :dexterity 5}})
+
+(def c-int (comp :intelligence :attributes))
+
+(def c-str (comp :strength :attributes))
+
+(def c-dex (comp :dexterity :attributes))
+
+(def attr (comp (character :attributes)))
+
+(defn n-int [char lvl] (update-in char [:attributes :intelligence] + lvl))
+
+(defn n-str [char lvl] (update-in char [:attributes :strength] + lvl))
+
+(defn n-dex [char lvl] (update-in char [:attributes :dexterity] + lvl))
+
+(defn my-comp [& all-fns]
+  (reduce
+    (fn [acc-fn curr-fn]
+      (fn [& args]
+        (acc-fn (apply curr-fn args))))
+    identity
+    all-fns))
+
+(defn my-assoc-in
+  [map [key & keys] values]
+  (if keys
+    (assoc map key (my-assoc-in (get map key) keys values))
+    (assoc map key values)))
+
+(defn my-update-in
+  ([map keys func & args]
+   (let [up (fn up [map keys func args]
+              (let [[key & keys] keys]
+                (if keys
+                  (assoc map key (up (get map key) keys func args))
+                  (assoc map key (apply func (get map key) args)))))]
+     (up map keys func args))))
---
+++ test/clojure_brave_and_true/chapter5_test.clj
@@ -0,0 +1,72 @@
+(ns clojure-brave-and-true.chapter5-test
+  (:require [clojure.test :refer :all :as t]
+            [clojure-brave-and-true.chapter5 :refer :all :as ch5]))
+
+(t/deftest c-int-test
+  (testing "should return character int"
+    (is (= (ch5/c-int ch5/character)
+           10))
+    ))
+
+(t/deftest c-dex-test
+  (testing "should return character dex"
+    (is (= (ch5/c-dex ch5/character)
+           5))
+    ))
+
+(t/deftest c-str-test
+  (testing "should return character str"
+    (is (= (ch5/c-str ch5/character)
+           4))
+    ))
+
+(t/deftest n-int-test
+  (testing "should return character next int"
+    (is (= (ch5/c-int (ch5/n-int ch5/character 1))
+           11))
+    ))
+
+(t/deftest n-dex-test
+  (testing "should return character next dex"
+    (is (= (ch5/c-dex (ch5/n-dex ch5/character 1))
+           6))
+    ))
+
+(t/deftest n-str-test
+  (testing "should return character next str"
+    (is (= (ch5/c-str (ch5/n-str ch5/character 1))
+           5))
+    ))
+
+
+(t/deftest n-str-test
+  (testing "should return character next str"
+    (is (= (ch5/c-str (ch5/n-str ch5/character 1))
+           5))
+    ))
+
+(t/deftest my-comp-test
+  (testing "my-comp should behave as comp"
+    (is (= ((ch5/my-comp str +) 8 8 8)
+           "24"))
+    (is (= (filter (ch5/my-comp not zero?) [0 1 0 2 0 3 0 4])
+           [1 2 3 4]))
+    ))
+
+(t/deftest my-assoc-in-test
+  (testing "my-assoc-in should behave as assoc-in"
+    (let [users [{:name "James" :age 26}  {:name "John" :age 43}]]
+      (is (= (ch5/my-assoc-in users [1 :age] 44)
+             [{:name "James", :age 26} {:name "John", :age 44}]))
+      (is (= (ch5/my-assoc-in users [1 :password] "nhoJ")
+             [{:name "James", :age 26} {:password "nhoJ", :name "John", :age 43}]))
+      )))
+
+(t/deftest my-update-in-test
+  (testing "my-update-in should behave as update-in"
+    (let [p {:name "James" :age 26}]
+      (is (= (update-in p [:age] inc)
+             {:name "James", :age 27}))
+      (is (= (update-in p [:age] + 10)
+             {:name "James", :age 36}))
+      )))
